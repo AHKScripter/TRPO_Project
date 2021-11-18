b39eb0ce6609f43592f05ec3e89a2fbb661250bc
Chikipooki <79464477+Chikipooki@users.noreply.github.com>
2021-11-18 14:28:36 +0700

    Update
    Co-Authored-By: AHKScripter <8450594+AHKScripter@users.noreply.github.com>

--- TRPO_Project/Learning.Designer.cs
+++ TRPO_Project/Learning.Designer.cs
@@ -29,135 +29,50 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Learning));
-            Bunifu.UI.WinForms.BunifuButton.BunifuButton2.BorderEdges borderEdges1 = new Bunifu.UI.WinForms.BunifuButton.BunifuButton2.BorderEdges();
             Bunifu.UI.WinForms.BunifuTextBox.StateProperties stateProperties1 = new Bunifu.UI.WinForms.BunifuTextBox.StateProperties();
             Bunifu.UI.WinForms.BunifuTextBox.StateProperties stateProperties2 = new Bunifu.UI.WinForms.BunifuTextBox.StateProperties();
             Bunifu.UI.WinForms.BunifuTextBox.StateProperties stateProperties3 = new Bunifu.UI.WinForms.BunifuTextBox.StateProperties();
             Bunifu.UI.WinForms.BunifuTextBox.StateProperties stateProperties4 = new Bunifu.UI.WinForms.BunifuTextBox.StateProperties();
-            this.label1 = new System.Windows.Forms.Label();
-            this.Learning_left_panel = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.Learning_add_button = new Bunifu.UI.WinForms.BunifuButton.BunifuButton2();
+            this.Learning_add_button = new Bunifu.UI.WinForms.BunifuImageButton();
             this.Learning_text_box = new Bunifu.UI.WinForms.BunifuTextBox();
+            this.Learning_list = new System.Windows.Forms.Panel();
+            this.Learning_left_panel = new System.Windows.Forms.Panel();
+            this.Learning_label = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(4, 2);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(285, 34);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "ToDo List | Learning";
-            // 
-            // Learning_left_panel
-            // 
-            this.Learning_left_panel.BackColor = System.Drawing.Color.DarkMagenta;
-            this.Learning_left_panel.Location = new System.Drawing.Point(4, 41);
-            this.Learning_left_panel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.Learning_left_panel.Name = "Learning_left_panel";
-            this.Learning_left_panel.Size = new System.Drawing.Size(20, 470);
-            this.Learning_left_panel.TabIndex = 1;
-            // 
-            // panel2
-            // 
-            this.panel2.Location = new System.Drawing.Point(30, 41);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(650, 470);
-            this.panel2.TabIndex = 2;
             // 
             // Learning_add_button
             // 
+            this.Learning_add_button.ActiveImage = global::TRPO_Project.Properties.Resources.plus_2_1;
             this.Learning_add_button.AllowAnimations = true;
-            this.Learning_add_button.AllowMouseEffects = true;
+            this.Learning_add_button.AllowBuffering = false;
             this.Learning_add_button.AllowToggling = false;
-            this.Learning_add_button.AnimationSpeed = 200;
-            this.Learning_add_button.AutoGenerateColors = false;
-            this.Learning_add_button.AutoRoundBorders = false;
-            this.Learning_add_button.AutoSizeLeftIcon = true;
-            this.Learning_add_button.AutoSizeRightIcon = true;
+            this.Learning_add_button.AllowZooming = true;
+            this.Learning_add_button.AllowZoomingOnFocus = false;
             this.Learning_add_button.BackColor = System.Drawing.Color.Transparent;
-            this.Learning_add_button.BackColor1 = System.Drawing.Color.DarkBlue;
-            this.Learning_add_button.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Learning_add_button.BackgroundImage")));
-            this.Learning_add_button.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton2.BorderStyles.Solid;
-            this.Learning_add_button.ButtonText = "";
-            this.Learning_add_button.ButtonTextMarginLeft = 0;
-            this.Learning_add_button.ColorContrastOnClick = 45;
-            this.Learning_add_button.ColorContrastOnHover = 45;
-            this.Learning_add_button.Cursor = System.Windows.Forms.Cursors.Default;
-            borderEdges1.BottomLeft = true;
-            borderEdges1.BottomRight = true;
-            borderEdges1.TopLeft = true;
-            borderEdges1.TopRight = true;
-            this.Learning_add_button.CustomizableEdges = borderEdges1;
             this.Learning_add_button.DialogResult = System.Windows.Forms.DialogResult.None;
-            this.Learning_add_button.DisabledBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
-            this.Learning_add_button.DisabledFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
-            this.Learning_add_button.DisabledForecolor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(160)))), ((int)(((byte)(168)))));
-            this.Learning_add_button.FocusState = Bunifu.UI.WinForms.BunifuButton.BunifuButton2.ButtonStates.Pressed;
-            this.Learning_add_button.Font = new System.Drawing.Font("Consolas", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Learning_add_button.ForeColor = System.Drawing.Color.White;
-            this.Learning_add_button.IconLeftAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.Learning_add_button.IconLeftCursor = System.Windows.Forms.Cursors.Default;
-            this.Learning_add_button.IconLeftPadding = new System.Windows.Forms.Padding(11, 3, 3, 3);
-            this.Learning_add_button.IconMarginLeft = 11;
-            this.Learning_add_button.IconPadding = 10;
-            this.Learning_add_button.IconRightAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.Learning_add_button.IconRightCursor = System.Windows.Forms.Cursors.Default;
-            this.Learning_add_button.IconRightPadding = new System.Windows.Forms.Padding(3, 3, 7, 3);
-            this.Learning_add_button.IconSize = 25;
-            this.Learning_add_button.IdleBorderColor = System.Drawing.Color.Transparent;
-            this.Learning_add_button.IdleBorderRadius = 1;
-            this.Learning_add_button.IdleBorderThickness = 1;
-            this.Learning_add_button.IdleFillColor = System.Drawing.Color.DarkBlue;
-            this.Learning_add_button.IdleIconLeftImage = global::TRPO_Project.Properties.Resources.plus_1;
-            this.Learning_add_button.IdleIconRightImage = null;
-            this.Learning_add_button.IndicateFocus = false;
-            this.Learning_add_button.Location = new System.Drawing.Point(580, 521);
-            this.Learning_add_button.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Learning_add_button.ErrorImage = ((System.Drawing.Image)(resources.GetObject("Learning_add_button.ErrorImage")));
+            this.Learning_add_button.FadeWhenInactive = false;
+            this.Learning_add_button.Flip = Bunifu.UI.WinForms.BunifuImageButton.FlipOrientation.Normal;
+            this.Learning_add_button.Image = global::TRPO_Project.Properties.Resources.plus_1_1;
+            this.Learning_add_button.ImageActive = global::TRPO_Project.Properties.Resources.plus_2_1;
+            this.Learning_add_button.ImageLocation = null;
+            this.Learning_add_button.ImageMargin = 0;
+            this.Learning_add_button.ImageSize = new System.Drawing.Size(59, 59);
+            this.Learning_add_button.ImageZoomSize = new System.Drawing.Size(60, 60);
+            this.Learning_add_button.InitialImage = ((System.Drawing.Image)(resources.GetObject("Learning_add_button.InitialImage")));
+            this.Learning_add_button.Location = new System.Drawing.Point(576, 355);
             this.Learning_add_button.Name = "Learning_add_button";
-            this.Learning_add_button.OnDisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
-            this.Learning_add_button.OnDisabledState.BorderRadius = 1;
-            this.Learning_add_button.OnDisabledState.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton2.BorderStyles.Solid;
-            this.Learning_add_button.OnDisabledState.BorderThickness = 1;
-            this.Learning_add_button.OnDisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
-            this.Learning_add_button.OnDisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(160)))), ((int)(((byte)(168)))));
-            this.Learning_add_button.OnDisabledState.IconLeftImage = null;
-            this.Learning_add_button.OnDisabledState.IconRightImage = null;
-            this.Learning_add_button.onHoverState.BorderColor = System.Drawing.Color.Transparent;
-            this.Learning_add_button.onHoverState.BorderRadius = 1;
-            this.Learning_add_button.onHoverState.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton2.BorderStyles.Solid;
-            this.Learning_add_button.onHoverState.BorderThickness = 1;
-            this.Learning_add_button.onHoverState.FillColor = System.Drawing.Color.DarkBlue;
-            this.Learning_add_button.onHoverState.ForeColor = System.Drawing.Color.White;
-            this.Learning_add_button.onHoverState.IconLeftImage = global::TRPO_Project.Properties.Resources.plus_1;
-            this.Learning_add_button.onHoverState.IconRightImage = null;
-            this.Learning_add_button.OnIdleState.BorderColor = System.Drawing.Color.Transparent;
-            this.Learning_add_button.OnIdleState.BorderRadius = 1;
-            this.Learning_add_button.OnIdleState.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton2.BorderStyles.Solid;
-            this.Learning_add_button.OnIdleState.BorderThickness = 1;
-            this.Learning_add_button.OnIdleState.FillColor = System.Drawing.Color.DarkBlue;
-            this.Learning_add_button.OnIdleState.ForeColor = System.Drawing.Color.White;
-            this.Learning_add_button.OnIdleState.IconLeftImage = global::TRPO_Project.Properties.Resources.plus_1;
-            this.Learning_add_button.OnIdleState.IconRightImage = null;
-            this.Learning_add_button.OnPressedState.BorderColor = System.Drawing.Color.Transparent;
-            this.Learning_add_button.OnPressedState.BorderRadius = 1;
-            this.Learning_add_button.OnPressedState.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton2.BorderStyles.Solid;
-            this.Learning_add_button.OnPressedState.BorderThickness = 1;
-            this.Learning_add_button.OnPressedState.FillColor = System.Drawing.Color.Aqua;
-            this.Learning_add_button.OnPressedState.ForeColor = System.Drawing.Color.White;
-            this.Learning_add_button.OnPressedState.IconLeftImage = global::TRPO_Project.Properties.Resources.plus_2;
-            this.Learning_add_button.OnPressedState.IconRightImage = null;
-            this.Learning_add_button.Size = new System.Drawing.Size(100, 60);
-            this.Learning_add_button.TabIndex = 4;
-            this.Learning_add_button.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.Learning_add_button.TextAlignment = System.Windows.Forms.HorizontalAlignment.Center;
-            this.Learning_add_button.TextMarginLeft = 0;
-            this.Learning_add_button.TextPadding = new System.Windows.Forms.Padding(0);
-            this.Learning_add_button.UseDefaultRadiusAndThickness = true;
+            this.Learning_add_button.Rotation = 0;
+            this.Learning_add_button.ShowActiveImage = true;
+            this.Learning_add_button.ShowCursorChanges = true;
+            this.Learning_add_button.ShowImageBorders = true;
+            this.Learning_add_button.ShowSizeMarkers = false;
+            this.Learning_add_button.Size = new System.Drawing.Size(60, 60);
+            this.Learning_add_button.TabIndex = 14;
+            this.Learning_add_button.ToolTipText = "";
+            this.Learning_add_button.WaitOnLoad = false;
+            this.Learning_add_button.Zoom = 0;
+            this.Learning_add_button.ZoomSpeed = 10;
             // 
             // Learning_text_box
             // 
@@ -167,7 +82,7 @@
             this.Learning_text_box.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.None;
             this.Learning_text_box.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.None;
             this.Learning_text_box.AutoSizeHeight = true;
-            this.Learning_text_box.BackColor = System.Drawing.Color.Transparent;
+            this.Learning_text_box.BackColor = System.Drawing.Color.White;
             this.Learning_text_box.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Learning_text_box.BackgroundImage")));
             this.Learning_text_box.BorderColorActive = System.Drawing.Color.DodgerBlue;
             this.Learning_text_box.BorderColorDisabled = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
@@ -187,7 +102,7 @@
             this.Learning_text_box.IconRight = null;
             this.Learning_text_box.IconRightCursor = System.Windows.Forms.Cursors.IBeam;
             this.Learning_text_box.Lines = new string[0];
-            this.Learning_text_box.Location = new System.Drawing.Point(30, 521);
+            this.Learning_text_box.Location = new System.Drawing.Point(30, 357);
             this.Learning_text_box.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Learning_text_box.MaxLength = 32767;
             this.Learning_text_box.MinimumSize = new System.Drawing.Size(2, 2);
@@ -224,9 +139,9 @@
             this.Learning_text_box.SelectionLength = 0;
             this.Learning_text_box.SelectionStart = 0;
             this.Learning_text_box.ShortcutsEnabled = true;
-            this.Learning_text_box.Size = new System.Drawing.Size(542, 60);
-            this.Learning_text_box.Style = Bunifu.UI.WinForms.BunifuTextBox._Style.Bunifu;
-            this.Learning_text_box.TabIndex = 3;
+            this.Learning_text_box.Size = new System.Drawing.Size(539, 58);
+            this.Learning_text_box.Style = Bunifu.UI.WinForms.BunifuTextBox._Style.Material;
+            this.Learning_text_box.TabIndex = 13;
             this.Learning_text_box.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
             this.Learning_text_box.TextMarginBottom = 0;
             this.Learning_text_box.TextMarginLeft = 3;
@@ -235,18 +150,46 @@
             this.Learning_text_box.UseSystemPasswordChar = false;
             this.Learning_text_box.WordWrap = true;
             // 
+            // Learning_list
+            // 
+            this.Learning_list.Location = new System.Drawing.Point(30, 43);
+            this.Learning_list.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Learning_list.Name = "Learning_list";
+            this.Learning_list.Size = new System.Drawing.Size(606, 304);
+            this.Learning_list.TabIndex = 12;
+            // 
+            // Learning_left_panel
+            // 
+            this.Learning_left_panel.BackColor = System.Drawing.Color.DarkMagenta;
+            this.Learning_left_panel.Location = new System.Drawing.Point(4, 43);
+            this.Learning_left_panel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Learning_left_panel.Name = "Learning_left_panel";
+            this.Learning_left_panel.Size = new System.Drawing.Size(18, 374);
+            this.Learning_left_panel.TabIndex = 11;
+            // 
+            // Learning_label
+            // 
+            this.Learning_label.AutoSize = true;
+            this.Learning_label.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Learning_label.Location = new System.Drawing.Point(4, 4);
+            this.Learning_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Learning_label.Name = "Learning_label";
+            this.Learning_label.Size = new System.Drawing.Size(285, 34);
+            this.Learning_label.TabIndex = 10;
+            this.Learning_label.Text = "ToDo List | Learning";
+            // 
             // Learning
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.Learning_add_button);
             this.Controls.Add(this.Learning_text_box);
-            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.Learning_list);
             this.Controls.Add(this.Learning_left_panel);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Learning_label);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Learning";
-            this.Size = new System.Drawing.Size(684, 586);
+            this.Size = new System.Drawing.Size(640, 420);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -254,10 +197,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Panel Learning_left_panel;
-        private System.Windows.Forms.Panel panel2;
+        private Bunifu.UI.WinForms.BunifuImageButton Learning_add_button;
         private Bunifu.UI.WinForms.BunifuTextBox Learning_text_box;
-        private Bunifu.UI.WinForms.BunifuButton.BunifuButton2 Learning_add_button;
+        private System.Windows.Forms.Panel Learning_list;
+        private System.Windows.Forms.Panel Learning_left_panel;
+        private System.Windows.Forms.Label Learning_label;
     }
 }
